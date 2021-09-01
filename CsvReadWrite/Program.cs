587dd731d425316eea1f4e97876258dcfaaab0f8
Lin <lin.special@gmail.com>
2021-09-01 15:31:01 -0400

    write

--- CsvReadWrite/Program.cs
+++ CsvReadWrite/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CsvReadWrite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,14 @@ namespace CsvReader
     {
         static void Main(string[] args)
         {
+            string p = @"G:\data\csv\test.csv";
+            using (CsvStreamWriter cw = new CsvStreamWriter(p, "|", "\r\n", "\"", "UTF-8", 5)) 
+            {
+                string[] f = { "A", "B", "C", "D", "E" };
+
+                cw.write(f, false, false);
+
+            }
         }
     }
 }
